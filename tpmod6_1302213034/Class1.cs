4cc3c89a5c1d3ab63e419e0a9326017ca3267516
Vanillateaa <mohamad.william24@gmail.com>
2023-03-27 00:17:10 +0700

    Initial commit

---
+++ tpmod6_1302213034/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpmod6_1302213034
+{
+    internal class Class1
+    {
+    }
+}
